69b2ddc30eb55c6548d192428bc6a96476796d4f
roopvirk6 <virkikroop13@gmail.com>
2020-04-08 01:18:47 -0600

    [ikroop] activity main updated with date picker

--- IKROOP/app/src/main/java/com/example/c0774174_w2020_mad3125_midterm/MainActivity.java
+++ IKROOP/app/src/main/java/com/example/c0774174_w2020_mad3125_midterm/MainActivity.java
@@ -59,7 +59,9 @@ public class MainActivity extends AppCompatActivity {
                 calendar.set(Calendar.YEAR, year);
                 calendar.set(Calendar.MONTH, monthOfYear);
                 calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                dateFormatDOB();
             };
+
 
 
         }
