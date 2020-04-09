441e2eb9986f4498e87d8e4112106c9e7091a7bd
roopvirk6 <virkikroop13@gmail.com>
2020-04-08 18:03:55 -0600

    [ikroop] acticity added

--- C0774174_W2020_MAD3125_MidTerm/app/src/main/java/com/example/c0774174_w2020_mad3125_midterm/SplashActivity.java
+++ C0774174_W2020_MAD3125_MidTerm/app/src/main/java/com/example/c0774174_w2020_mad3125_midterm/SplashActivity.java
@@ -1,14 +1,32 @@
 package com.example.c0774174_w2020_mad3125_midterm;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private final int duration = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        },duration);
+
+
     }
 }
